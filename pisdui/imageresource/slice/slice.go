04750ac450839c14b7eea1a8c56fb3225da06021
fabulousduck <ryanvlaming@icloud.com>
2018-09-11 14:10:23 +0200

    add slice block structure contents

--- pisdui/imageresource/slice/slice.go
+++ pisdui/imageresource/slice/slice.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	"github.com/fabulousduck/pisdui/pisdui/imageresource/descriptor"
+	"github.com/fabulousduck/pisdui/pisdui/util/shape"
 )
 
 type HeaderInterface interface {
@@ -12,7 +13,7 @@ type HeaderInterface interface {
 
 type Slice struct {
 	Header *HeaderInterface
-	Block  *Block
+	Block  []*Block
 }
 
 type HeaderCS6 struct {
@@ -24,10 +25,34 @@ type HeaderCS6 struct {
 //HeaderCS7 is for both CS7 and CS8.
 //it is denoted to 7 since that changes from 6, where 7 and 8 are the same
 type HeaderCS7 struct {
-	Version uint32
+	Version           uint32
+	BoundingRectangle *shape.Rectangle
+	Name              string
+	NumSlices         uint32
 }
 
 type Block struct {
+	ID                  uint32
+	GroupID             uint32
+	Origin              uint32
+	AssocLayerID        uint32
+	Name                string
+	Type                uint32
+	Dimensions          *shape.Rectangle
+	Url                 string
+	Target              string
+	Message             string
+	AltTag              string
+	CellTextIsHTML      bool
+	CellText            string
+	HorizontalAlignment uint32
+	VerticalAlignment   uint32
+	AlphaColor          int
+	Red                 int
+	Green               int
+	Blue                int
+	DescriptorVersion   uint32
+	Descriptor          *descriptor.Descriptor
 }
 
 func (headerCS6 HeaderCS6) GetHeaderVersion() uint16 {
