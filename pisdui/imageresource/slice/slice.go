71c3411c2880ab6c907edd97b0598d24fe5d778e
fabulousduck <ryanvlaming@icloud.com>
2018-09-18 22:58:36 +0200

    start on slice block parsing

--- pisdui/imageresource/slice/slice.go
+++ pisdui/imageresource/slice/slice.go
@@ -3,6 +3,8 @@ package slice
 import (
 	"os"
 
+	"github.com/davecgh/go-spew/spew"
+
 	"github.com/fabulousduck/pisdui/pisdui/imageresource/descriptor"
 	"github.com/fabulousduck/pisdui/pisdui/imageresource/slice/header"
 	util "github.com/fabulousduck/pisdui/pisdui/util/file"
@@ -54,7 +56,13 @@ func (slice *Slice) Parse(file *os.File) {
 
 	switch headerVersion {
 	case 6:
-
+		//TODO: debug why this runs into an EOF
+		//probably something todo with offset being incorrect
+		//when reading beginsS
+		spew.Dump(file.Seek(0, 1))
+		blockObject := NewBlock()
+		blockObject.Parse(file)
+		spew.Dump(blockObject)
 		break
 	case 7:
 		fallthrough
@@ -69,5 +77,34 @@ func NewBlock() *Block {
 }
 
 func (block *Block) Parse(file *os.File) {
+	dimensionsObject := shape.NewRectangle()
+	// descriptorObject := descriptor.NewDescriptor()
 
+	block.ID = util.ReadBytesLong(file)
+	block.GroupID = util.ReadBytesLong(file)
+	block.Origin = util.ReadBytesLong(file)
+	block.AssocLayerID = util.ReadBytesLong(file)
+	block.Name = util.ParseUnicodeString(file)
+	block.Type = util.ReadBytesLong(file)
+
+	dimensionsObject.Parse(file)
+
+	block.Dimensions = dimensionsObject
+	// block.Url = util.ParseUnicodeString(file)
+	// block.Target = util.ParseUnicodeString(file)
+	// block.Message = util.ParseUnicodeString(file)
+	// block.AltTag = util.ParseUnicodeString(file)
+	// block.CellTextIsHTML = util.ReadSingleByte(file) == 1
+	// block.CellText = util.ParseUnicodeString(file)
+	// block.HorizontalAlignment = util.ReadBytesLong(file)
+	// block.VerticalAlignment = util.ReadBytesLong(file)
+	// block.AlphaColor = util.ReadSingleByte(file)
+	// block.Red = util.ReadSingleByte(file)
+	// block.Green = util.ReadSingleByte(file)
+	// block.Blue = util.ReadSingleByte(file)
+	// block.DescriptorVersion = util.ReadBytesLong(file)
+
+	// descriptorObject.Parse(file)
+
+	// block.Descriptor = descriptorObject
 }
